700a265b8ce0402222dc9e2a00eaa7c850c9ba35
kols <kols@irace.com>
2017-04-18 20:29:04 +0300

    server: multipart-form data parsing & saving added

--- server/app.js
+++ server/app.js
@@ -3,8 +3,21 @@ const route = require("koa-route");
 const serve = require("koa-static");
 const fs = require("mz/fs");
 const path = require("path");
+const multer = require("koa-multer");
+const compose = require("koa-compose");
 
 const app = new Koa();
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./public/img/");
+  },
+  filename: (req, file, cb) => {
+    const fileExtension = file.originalname.split(".").pop();
+    cb(null, `${file.fieldname}.${fileExtension}`);
+  }
+});
+const upload = multer({ storage });
+
 function main() {
   app.use(async (ctx, next) => {
     try {
@@ -17,27 +30,30 @@ function main() {
 
   // TODO: diverse the middleware into different handler files
   app.use(serve(path.join(__dirname, "./../public")));
-
   app.use(
-    route.post("/settings", async ctx => {
-      let data;
-      try {
-        // TODO: utilize busboy here or some other module koa-multer, koa-better-body
-      } catch (e) {
-        throw e;
-      }
+    route.post(
+      "/settings",
+      compose([
+        upload.single("file"),
+        async ctx => {
+          try {
+            const file = fs.createWriteStream("./server/db/settings.json", {
+              autoClose: true
+            });
 
-      /* try {
-        const file = fs.createWriteStream("./db/settings.json", {
-          autoClose: true
-        });
-        await file.end(data);
-      } catch (e) {
-        throw e;
-      }*/
+            file.end(
+              JSON.stringify(
+                Object.assign({ fileUrl: ctx.req.file.path }, ctx.req.body)
+              )
+            );
+          } catch (e) {
+            throw e;
+          }
 
-      ctx.status = 200;
-    })
+          ctx.status = 200;
+        }
+      ])
+    )
   );
 
   app.use(
